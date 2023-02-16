a080addb508cd26851c383dc471f1ced17ac68f0
AndreuMB <andreuolleria@gmail.com>
2023-02-16 18:59:42 +0100

    enemy behaviour

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float life = 2;
+    Transform player;
+    [SerializeField] float speed = 10;
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        Bullet.hit.AddListener(damage);
+        // Bullet.hit.AddListener(damage);
+        player = GameObject.Find("Player").transform;
+        rb = GetComponent<Rigidbody>();
     }
 
     void damage(){
@@ -34,6 +39,18 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.LookAt(player);
+        float distance = Vector3.Distance (gameObject.transform.position, player.transform.position);
+        if (distance<5)
+        {
+            rb.velocity = Vector3.zero;
+            print("fire");
+        }else{
+            rb.velocity = transform.forward * speed;
+        }
+    }
+
+    void FixedUpdate(){
+
     }
 }
