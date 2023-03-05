67307c0866af573957c8a4f4d0eb30a054574c3e
abastos-fila2 <roberth.e.rrc@gmail.com>
2023-03-05 15:36:46 +0100

    TM PRO, Arreglo RotaciónJugador, TODO Recarga Arma

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -97,14 +97,14 @@ public class PlayerController : Character
         float prevAngleX = Quaternion.Angle(transform.rotation, Camera.main.transform.rotation * q);
 
         //Rotamos la camara en vertical si esta dentro del rango permitido
-        if (prevAngleX > LIMIT_ANGLE)
+        if (prevAngleX < LIMIT_ANGLE)
         {
-            // camara.transform.rotation *= q;
-            rotacionY = 0;
+            camara.transform.rotation *= q;
+            //rotacionY = 0;
         }
 
         //Rotamos al personaje en el eje Y
-        Vector3 rotacionJugador = new Vector3(rotacionY + transform.eulerAngles.x, rotacionX+transform.eulerAngles.y, transform.eulerAngles.z);
+        Vector3 rotacionJugador = new Vector3(transform.eulerAngles.x, rotacionX+transform.eulerAngles.y, transform.eulerAngles.z);
         transform.rotation = Quaternion.Euler(rotacionJugador);
 
     }
@@ -154,16 +154,5 @@ public class PlayerController : Character
         }
         selectedWeapon.gameObject.SetActive(true);
     }
-
-    void InstanciaArmas()
-    {
-        for (int i = 0; i < weapons.Count; i++)
-        {
-            weapons[i] = Instantiate(weapons[i].gameObject, transform.GetChild(0).transform.GetChild(0).transform.position, Quaternion.identity, transform.GetChild(0).transform.GetChild(0).transform).GetComponent<Weapon>();
-            weapons[i].gameObject.SetActive(false);
-        }
-        weapons[0].gameObject.SetActive(true);
-    }
-
 
 }
