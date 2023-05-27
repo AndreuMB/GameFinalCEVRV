45976625e952525e71885f08c3b026dac39e7a83
AndreuMB <andreuolleria@gmail.com>
2023-05-27 02:17:20 +0200

    lake tp

--- Assets/Scripts/RandomizeSpawn.cs
+++ Assets/Scripts/RandomizeSpawn.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class RandomizeSpawn : MonoBehaviour
 {
+    [SerializeField] bool singleInstance;
     // Start is called before the first frame update
     void Start()
     {
