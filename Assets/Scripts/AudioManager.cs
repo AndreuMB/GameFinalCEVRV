2610c11ea95b179921b2b5fdcc4fed8b32336dd4
AndreuMB <andreuolleria@gmail.com>
2023-05-28 19:56:04 +0200

    fixes material enemy 2 and audio manager music menu

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -9,13 +9,13 @@ public class AudioManager : MonoBehaviour
 
     void Awake(){
 
-        if (instance == null)
-        {
-            instance = this;
-        }else{
-            Destroy(gameObject);
-            return;
-        }
+        // if (instance == null)
+        // {
+        //     instance = this;
+        // }else{
+        //     Destroy(gameObject);
+        //     return;
+        // }
 
         // DontDestroyOnLoad(this.gameObject);
         
@@ -43,6 +43,7 @@ public class AudioManager : MonoBehaviour
 
     void Start(){
         Scene scene = SceneManager.GetActiveScene();
+        print("scene = " + scene.buildIndex);
         if (scene.buildIndex == 0)
         {
             Play("ThemeMM");
